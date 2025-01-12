3a9b6dc7c876dce006234b644372bc2a71f9bc51
ummeFA <ummefatema33@gmail.com>
2025-01-12 23:55:34 +0900

    backend code to save-working on it

--- frontend/flashcard/src/components/AddVocabulary.jsx
+++ frontend/flashcard/src/components/AddVocabulary.jsx
@@ -1,11 +1,20 @@
 import { faFloppyDisk } from "@fortawesome/free-regular-svg-icons";
 import { faChevronCircleLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const AddVocabulary = () => {
   const navigate = useNavigate();
+
+  // Vocabulary state
+  const [newKanji, setNewKanji] = useState("");
+  const [newHiragana, setNewHiragana] = useState("");
+  const [newEnglish, setNewEnglish] = useState("");
+
+  const handleSave = async () => {
+    const data = { newKanji, newHiragana, newEnglish };
+  };
 
   const previousPage = () => {
     navigate(-1);
@@ -16,17 +25,24 @@ const AddVocabulary = () => {
       <div className="flex flex-col text-xl w-2/3">
         <input
           type="text"
-          placeholder="enter Kanji..."
+          placeholder="Enter Kanji..."
+          onChange={(e) => setNewKanji(e.target.value)}
           className="border border-black rounded p-3 mb-3"
         />
         <input
           type="text"
-          placeholder="enter Hiragana..."
+          placeholder="Enter Hiragana..."
+          onChange={(e) => {
+            setNewHiragana(e.target.value);
+          }}
           className="border border-black rounded p-3 mb-3"
         />
         <input
           type="text"
-          placeholder="enter English..."
+          placeholder="Enter English..."
+          onChange={(e) => {
+            setNewEnglish(e.target.value);
+          }}
           className="border border-black rounded p-3 mb-3"
         />
       </div>
@@ -34,8 +50,8 @@ const AddVocabulary = () => {
       {/* Buttons */}
       <div className="flex flex-col md:flex-row justify-center items-center gap-3 mt-5 w-2/3">
         <button
-          className="bg-green-600 text-white flex items-center justify-center p-3 border rounded-lg w-full md:w-1/2"
-          onClick={previousPage}
+          className="bg-green-600 text-white flex items-center justify-center p-3 border rounded-lg w-full md:w-1/2 hover:bg-green-800"
+          onClick={handleSave}
         >
           <FontAwesomeIcon
             icon={faFloppyDisk}
@@ -43,7 +59,7 @@ const AddVocabulary = () => {
           />
           <span className="hidden md:block ml-2 text-xl"> Save</span>
         </button>
-        <button className="bg-blue-600 text-white flex items-center justify-center p-3 border rounded-lg w-full md:w-1/2">
+        <button className="bg-blue-600 text-white flex items-center justify-center p-3 border rounded-lg w-full md:w-1/2 hover:bg-blue-800">
           <FontAwesomeIcon
             icon={faChevronCircleLeft}
             className="text-xl md:text-2xl"
